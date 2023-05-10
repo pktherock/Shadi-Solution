ea54ebdf173216e9515af785ada0fdf636f84133
pktherock <pktherock00@gmail.com>
2023-05-10 22:35:05 +0530

    footer link changed to next js link

--- src/components/layouts/footer/footer.tsx
+++ src/components/layouts/footer/footer.tsx
@@ -11,10 +11,11 @@ const Footer: FC<FooterProps> = () => {
         <div className="sm:flex sm:items-center sm:justify-between">
           <Link href="#home" className="flex items-center">
             <Image
-              src="https://flowbite.com/docs/images/logo.svg"
+              src="/assets/img/brand/brand_logo.jpeg"
               alt="Brand Logo"
               width={30}
               height={30}
+              className="rounded-full"
             />
             <span className="pl-1 self-center text-1xl font-semibold whitespace-nowrap dark:text-white">
               Shadi Solution
@@ -23,24 +24,24 @@ const Footer: FC<FooterProps> = () => {
           {/* // todo */}
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
-              <a href="#" className="mr-4 hover:underline md:mr-6 ">
+              <Link href="#" className="mr-4 hover:underline md:mr-6 ">
                 About
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="#" className="mr-4 hover:underline md:mr-6">
+              <Link href="#" className="mr-4 hover:underline md:mr-6">
                 Privacy Policy
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="#" className="mr-4 hover:underline md:mr-6 ">
+              <Link href="#" className="mr-4 hover:underline md:mr-6 ">
                 Licensing
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="#" className="hover:underline">
+              <Link href="#" className="hover:underline">
                 Contact
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
@@ -48,13 +49,13 @@ const Footer: FC<FooterProps> = () => {
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2023{' '}
-            <a href="https://flowbite.com/" className="hover:underline">
+            <Link href="#" className="hover:underline">
               Shadi Solution
-            </a>
+            </Link>
             . All Rights Reserved.
           </span>
           <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-            <a
+            <Link
               href="#"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
             >
@@ -71,8 +72,8 @@ const Footer: FC<FooterProps> = () => {
                 />
               </svg>
               <span className="sr-only">Facebook page</span>
-            </a>
-            <a
+            </Link>
+            <Link
               href="#"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
             >
@@ -89,8 +90,8 @@ const Footer: FC<FooterProps> = () => {
                 />
               </svg>
               <span className="sr-only">Instagram page</span>
-            </a>
-            <a
+            </Link>
+            <Link
               href="#"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
             >
@@ -103,8 +104,8 @@ const Footer: FC<FooterProps> = () => {
                 <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
               </svg>
               <span className="sr-only">Twitter page</span>
-            </a>
-            <a
+            </Link>
+            <Link
               href="#"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
             >
@@ -121,7 +122,7 @@ const Footer: FC<FooterProps> = () => {
                 />
               </svg>
               <span className="sr-only">GitHub account</span>
-            </a>
+            </Link>
           </div>
         </div>
       </div>
